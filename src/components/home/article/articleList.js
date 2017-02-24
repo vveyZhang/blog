3d69bc8ee140a7c1f6529ff6e56ae7c851a3eadd
hellosmallwei <helloxwz@qq.com>
2017-02-24 13:41:26 +0800

    加载优化

--- src/components/home/article/articleList.js
+++ src/components/home/article/articleList.js
@@ -38,14 +38,13 @@ export class HomeArticleList extends React.Component{
             var ch=$(window).height();
             var dh=$(document).height();
             var top=$(window).scrollTop();
-            console.log(that.state.ready);
-
-            if(top+ch==dh){
+            console.log(that.state.ready+"初始状态");
+            if(top+ch>=dh){
                 that.setState({
                     loading:true,
                     page:that.state.page+1
                 });
-                console.log(that.state.loading);
+                console.log(that.state.loading+"是否加载");
                 $.ajax({
                     type:'get',
                     url:'/handle/home/findtype',
