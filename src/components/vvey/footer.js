e6f412686660f549017bd5bd161210cc5ef47cf9
hellosmallwei <helloxwz@qq.com>
2017-04-26 13:35:44 +0800

    添加备案

--- src/components/vvey/footer.js
+++ src/components/vvey/footer.js
@@ -6,7 +6,7 @@ export class Footer extends React.Component{
             <div id="site-footer">
                 <div className="">
                     <div className="col-md-12">
-                        <p className="copyright">&copy; 2017 vveyzhang.com  </p>
+                        <p className="copyright">&copy; 2017 vveyzhang.com   备案号：蜀ICP备17011378号-1 </p>
                     </div>
                 </div>
             </div>
